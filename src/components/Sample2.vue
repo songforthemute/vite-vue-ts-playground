78a3ddd8826be5f13037232c0fd29a430c7fb61a
songforthemute <huni0423@naver.com>
2024-03-11 04:19:19 +0900

    practice vue

---
+++ src/components/Sample2.vue
@@ -0,0 +1,32 @@
+<template>
+  <Sample2Header :appTitle="msg" @change="changeMessage" />
+  <button @click="showAlert">경고</button>
+</template>
+
+<script lang="ts">
+import Sample2Header from "./Sample2-header.vue";
+
+  export default {
+    components: {
+      Sample2Header,
+    }
+    ,data() {
+      return {
+        msg: '앱 헤더 컴포넌트'
+      }
+    },
+    methods: {
+      showAlert() {
+        alert('hello!')
+      },
+      changeMessage() {
+        this.msg = '변경 완료'
+      }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
